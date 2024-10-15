6077817aec6c72092475b84d7ca85f36e4b730cc
Shourya Kumar Singh <shouryak647@gmail.com>
2024-10-16 00:12:46 +0530

    restoring the server.js file

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,6 @@ const app = express();
 app.use(express.json()); // to accept json data
 app.set('trust proxy', true);
 
-
-
 const corsOptions = {
   origin: ["https://www.textrandom.online", "https://textrandom.online"],
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -35,3 +33,98 @@ require('./backup');  // Include the backup task here
 // --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+// IP address middleware to handle cases where the app is behind a proxy
+app.use((req, res, next) => {
+  const forwarded = req.headers['x-forwarded-for'];
+  req.ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+  next();
+});
+
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: ["https://www.textrandom.online", "https://textrandom.online", "https://api.textrandom.online"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+});
+
+let onlineUsers = {};
+
+
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.on('userConnected', (userId) => {
+    onlineUsers[userId] = socket.id;; // Store the socket id with the user id
+    io.emit('updateOnlineUsers', onlineUsers); // Notify everyone
+
+  });
+  socket.on('disconnect', () => {
+
+    const disconnectedUserId = Object.keys(onlineUsers).find(id => onlineUsers[id] === socket.id);
+    if (disconnectedUserId) {
+      delete onlineUsers[disconnectedUserId];
+      io.emit('updateOnlineUsers', onlineUsers); // Notify everyone
+    }
+
+
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
